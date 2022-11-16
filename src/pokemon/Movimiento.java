c74d4d630cfb18366b42b607caf5dc6e897943e5
olaba29 <andoni.ola@gmail.com>
2022-11-16 14:03:19 +0100

    Movimiento preview (es el movimiento base, tenemos que añadir las funcionalidades)

--- src/pokemon/Movimiento.java
+++ src/pokemon/Movimiento.java
@@ -1,4 +1,38 @@
 package pokemon;
+public abstract class Movimiento {
+    //atributuak
+    private int tipo;
+    private String descripcion;
+    private int id;
+    private int precio;
+    //eraikitzailea
+    public Movimiento(int pTipo, String pDescripcion, int pId,int pPrecio) {
+        this.tipoa = pTipo;
+        this.deskripzioa = pDescripcion;
+        this.id = pId;
+        this.prezioa = pPrecio;
+    }
+    //gainontzeko metodoak
+    public int getTipo() {
+        return this.tipo;
+    }
 
-public class Movimiento {
+    public int getId() {
+        return this.id;
+    }
+    public int getPrecio() {
+        return this.precio;
+    }
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public boolean idIgual(int pId) {
+        boolean res = false;
+        if(this.getId() == pId) {
+            res = true;
+        }
+        return res;
+    }
+
 }
