5d349c9b21550631b92115bab03adb7e169bf078
gorkadra <gdelrio004@ikasle.ehu.eus>
2022-11-18 01:16:45 +0100

    Lector adaptado a todos los casos y clase Tabla de tipos

--- src/txt/Lector.java
+++ src/txt/Lector.java
@@ -1,7 +1,11 @@
 package txt;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
+
+import pokemon.*;
+
 
 public class Lector {
 
@@ -9,47 +13,25 @@ public class Lector {
         try{
             Scanner sc=new Scanner(new FileReader("src/txt/Tipos.txt"));
 
+            int linea;
+            int columna;
+            float valor;
+
             while(sc.hasNext()){
-
-                if(mailaSt.equals(sc.nextLine())){
-                    aurkituta=true;
+                String lineatxt=sc.nextLine();
+                String[] listadeLinea = lineatxt.split(",");
+                if(listadeLinea.length==3){
+                    linea = Integer.parseInt(listadeLinea[0]);
+                    columna = Integer.parseInt(listadeLinea[1]);
+                    valor = (float) Integer.parseInt(listadeLinea[2]);
+                    TablaTipos.getMiTablaTipos().setNum(linea,columna,valor);
                 }
-
-            }
-
-            //tableroa kargatu
-            int err=0;
-            while(err<9){
-                String linea=sc.nextLine();
-                int zut=0;
-                while(zut<9){
-                    char c=linea.charAt(zut);
-                    String str=String.valueOf(c);
-                    sudoku[err][zut]=Integer.parseInt(str);
-                    sudokuStart[err][zut]=Integer.parseInt(str);
-                    zut++;
+                else{
+                    sc.nextLine();
                 }
-                err++;
             }
-
-
-            //tablero emaitza kargatu
-            int err_2=0;
-            while(err_2<9){
-                String linea=sc.nextLine();
-                int zut_2=0;
-                while(zut_2<9){
-                    char c=linea.charAt(zut_2);
-                    String str=String.valueOf(c);
-                    sudokuEma[err_2][zut_2]=Integer.parseInt(str);
-                    zut_2++;
-                }
-                err_2++;
-            }
-
-
         }catch (FileNotFoundException e) {
-            System.out.println("Ez da aurkitu fitxategia");
+            System.out.println("No se ha encontrado el txt");
 
         }
     }
@@ -57,49 +39,38 @@ public class Lector {
     public void leerPokemons(){
         try{
             Scanner sc=new Scanner(new FileReader("src/txt/Pokemons.txt"));
-
-
+            String nombre;
+            int id;
+            float vida;
+            float ataque;
+            float velocidad;
+            float defensa;
+            int tipo;
+            float ataquesp;
+            float defensasp;
             while(sc.hasNext()){
-
-                if(mailaSt.equals(sc.nextLine())){
-                    aurkituta=true;
-                }
-
-            }
-
-            //tableroa kargatu
-            int err=0;
-            while(err<9){
                 String linea=sc.nextLine();
-                int zut=0;
-                while(zut<9){
-                    char c=linea.charAt(zut);
-                    String str=String.valueOf(c);
-                    sudoku[err][zut]=Integer.parseInt(str);
-                    sudokuStart[err][zut]=Integer.parseInt(str);
-                    zut++;
+                String[] listadeLinea = linea.split(",");
+                if(listadeLinea.length==9){
+                    nombre = listadeLinea[0];
+                    id = Integer.parseInt(listadeLinea[1]);
+                    vida = (float) Integer.parseInt(listadeLinea[2]);
+                    ataque = (float) Integer.parseInt(listadeLinea[3]);
+                    velocidad = (float) Integer.parseInt(listadeLinea[4]);
+                    defensa = (float) Integer.parseInt(listadeLinea[5]);
+                    tipo = Integer.parseInt(listadeLinea[6]);
+                    ataquesp = (float) Integer.parseInt(listadeLinea[7]);
+                    defensasp = (float) Integer.parseInt(listadeLinea[8]);
+
+                    Pokemon poke = new Pokemon(nombre,id,vida,ataque,velocidad,defensa,tipo,ataquesp,defensasp);
+                    Pokedex.getMiPokedex().getMiLista().addPokemon(poke);
                 }
-                err++;
-            }
-
-
-            //tablero emaitza kargatu
-            int err_2=0;
-            while(err_2<9){
-                String linea=sc.nextLine();
-                int zut_2=0;
-                while(zut_2<9){
-                    char c=linea.charAt(zut_2);
-                    String str=String.valueOf(c);
-                    sudokuEma[err_2][zut_2]=Integer.parseInt(str);
-                    zut_2++;
+                else{
+                    sc.nextLine();
                 }
-                err_2++;
             }
-
-
         }catch (FileNotFoundException e) {
-            System.out.println("Ez da aurkitu fitxategia");
+            System.out.println("No se ha encontrado el txt");
 
         }
     }
@@ -107,52 +78,64 @@ public class Lector {
     public void leerAtaques(){
         try{
             Scanner sc=new Scanner(new FileReader("src/txt/Ataques.txt"));
-
+            int tipo;
+            String descripcion;
+            int id;
+            String cat;
+            boolean catBool;
+            int potencia;
+            float booster;
+            String estadistica;
+            String propio
+            boolean propioBool;
             while(sc.hasNext()){
                 String linea=sc.nextLine();
                 String[] listadeLinea = linea.split(",");
-                int i;
-                String palabra;
-                for ( i=0, i++, i<listadeLinea.length){
-                    palabra = listadeLinea[i];
+
+                if(listadeLinea.length==5){
+                    tipo = Integer.parseInt(listadeLinea[0]);
+                    descripcion = listadeLinea[1];
+                    id = Integer.parseInt(listadeLinea[2]);
+                    cat = listadeLinea[3];
+                    if (cat=="true"){
+                        catBool=true;
+                    }
+                    else{
+                        catBool=false;
+                    }
+                    potencia = Integer.parseInt(listadeLinea[4]);
+                    MovimientoAtaque movAtac = new MovimientoAtaque(tipo,descripcion,id,catBool,potencia);
+                    ColeccionMovi.getMiListaMovi().getMiLista().addMovi(movAtac);
                 }
-
-            }
-
-            //tableroa kargatu
-            int err=0;
-            while(err<9){
-                String linea=sc.nextLine();
-                String[] listadeLinea = linea.split(",");
-                int zut=0;
-                while(zut<9){
-                    char c=linea.charAt(zut);
-                    String str=String.valueOf(c);
-                    sudoku[err][zut]=Integer.parseInt(str);
-                    sudokuStart[err][zut]=Integer.parseInt(str);
-                    zut++;
+                else if(listadeLinea.length==7){
+                    tipo = Integer.parseInt(listadeLinea[0]);
+                    descripcion = listadeLinea[1];
+                    id = Integer.parseInt(listadeLinea[2]);
+                    cat = listadeLinea[3]);
+                    if (cat=="true"){
+                        catBool=true;
+                    }
+                    else{
+                        catBool=false;
+                    }
+                    booster = (float) Integer.parseInt(listadeLinea[4]);
+                    estadistica = listadeLinea[5];
+                    propio =  listadeLinea[6];
+                    if (propio=="true"){
+                        propioBool=true;
+                    }
+                    else{
+                        propioBool=false;
+                    }
+                    MovimientoStat movStat = new MovimientoStat(tipo,descripcion,id,catBool,booster,estadistica,propioBool);
+                    ColeccionMovi.getMiListaMovi().getMiLista().addMovi(movStat);
                 }
-                err++;
-            }
-
-
-            //tablero emaitza kargatu
-            int err_2=0;
-            while(err_2<9){
-                String linea=sc.nextLine();
-                int zut_2=0;
-                while(zut_2<9){
-                    char c=linea.charAt(zut_2);
-                    String str=String.valueOf(c);
-                    sudokuEma[err_2][zut_2]=Integer.parseInt(str);
-                    zut_2++;
+                else{
+                    sc.nextLine();
                 }
-                err_2++;
             }
-
-
         }catch (FileNotFoundException e) {
-            System.out.println("Ez da aurkitu fitxategia");
+            System.out.println("No se ha encontrado el txt");
 
         }
     }
