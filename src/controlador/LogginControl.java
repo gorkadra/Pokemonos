086891c199f521416973376f69e125592d0df3c3
gorkadra <gdelrio004@ikasle.ehu.eus>
2022-11-20 10:44:28 +0100

    Si le das a salir en el log cierra el progrema del todo

--- src/controlador/LogginControl.java
+++ src/controlador/LogginControl.java
@@ -6,6 +6,7 @@ import vista.Loggin;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 public class LogginControl implements ActionListener {
 
@@ -36,6 +37,7 @@ public class LogginControl implements ActionListener {
         }else if(log.getSalir().equals(e.getSource())){
             System.out.println("pilla el boton salir");
             //Hacer que se cierre la ventana
+            System.exit(0);
         }
 
 
